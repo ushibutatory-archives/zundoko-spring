94feb5bcbc6831319f49f6de888feb36e053d558
ushibutatory <ushibutatory@gmail.com>
2016-12-19 00:06:50 +0900

    refactor: コメント補足

--- src/main/java/com/ushibutatory/zundoko/model/Result.java
+++ src/main/java/com/ushibutatory/zundoko/model/Result.java
@@ -11,20 +11,49 @@ import java.util.List;
 @Data
 public class Result {
 
+    /**
+     * 試行回数
+     */
     private int count;
 
+    /**
+     * 結果フレーズリスト
+     */
     private List<String> results;
 
+    /**
+     * コンストラクタ
+     */
     public Result() {
         this.results = new ArrayList<>();
     }
 
+    /**
+     * フレーズを追加します。
+     *
+     * @param phrase フレーズ
+     */
     public void addPhrase(String phrase) {
         this.results.add(phrase);
     }
 
+    /**
+     * 完成フレーズを取得します。
+     *
+     * @return 完成フレーズ
+     */
     public String phrases() {
-        return String.join("", this.results);
+        return this.phrases("");
+    }
+
+    /**
+     * 完成フレーズを取得します。
+     *
+     * @param separator 区切り文字
+     * @return 完成フレーズ
+     */
+    public String phrases(String separator) {
+        return String.join(separator, this.results);
     }
 
 }
--- src/main/java/com/ushibutatory/zundoko/rest/AlbumRestController.java
+++ src/main/java/com/ushibutatory/zundoko/rest/AlbumRestController.java
@@ -2,15 +2,22 @@ package com.ushibutatory.zundoko.rest;
 
 import com.ushibutatory.zundoko.model.Album;
 import com.ushibutatory.zundoko.model.ISong;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * アルバムAPI
+ */
 @RestController
 @RequestMapping("/api/album")
 public class AlbumRestController {
 
+    /**
+     * すべての歌を取得します。
+     *
+     * @return 歌リスト
+     */
     @RequestMapping(value = "/all", method = RequestMethod.GET)
     public List<ISong> all() {
         return Album.getInstance().findAll();
